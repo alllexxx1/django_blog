4176a683a203e20df1115cb39fc5a9642d8560f9
alllexxx1 <alekkorpachyov@gmail.com>
2023-12-06 17:48:28 +0300

    Add redirect through url got by "reverse"

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django_blog.article import views
 
+
 urlpatterns = [
     path('', views.IndexView.as_view()),
+    path('<str:tags>/<int:article_id>/', views.ArticleView.as_view(), name='article'),
 ]
--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,8 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
+from django.urls import reverse
+from django.shortcuts import redirect
+
+
+class ArticleView(View):
+
+    def get(self, request, tags, article_id):
+        response_text = f'Статья номер {article_id}. Тег {tags}'
+        return HttpResponse(response_text)
+
+
+# def index(request):
+#     # url = reverse('article', args=['python', 42])
+#     url = reverse('article', kwargs={'tags': 'PYTHON', 'article_id': 42})
+#     return redirect(url)
 
 
 class IndexView(View):
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Articles')
+    def get(self, request):
+        url = reverse('article', kwargs={'tags': 'PYTHON', 'article_id': 42})
+        return HttpResponseRedirect(url)
