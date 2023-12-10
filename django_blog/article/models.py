d3bd7425f29ce355da11b2eb4ab1d6c2e713275e
alllexxx1 <alekkorpachyov@gmail.com>
2023-12-10 18:14:59 +0300

    Add "python_dotenv" for managing configs. Tune up admin board

--- django_blog/article/models.py
+++ django_blog/article/models.py
@@ -5,3 +5,10 @@ class Article(models.Model):
     name = models.CharField(max_length=200)
     body = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+# Article.objects.bulk_create([Article(name='magic article', body='some info about MAGIC article'), Article(name='usual article', body='some info about USUAL article'),
+#                              Article(name='unusual article', body='some info about UNUSUAL article'), Article(name='mega article', body='some info about MEGA article'),
+#                              Article(name='tremendous article', body='some info about TREMENDOUS article')])
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Loading of env variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z4zp=@ortjof1+(&d!4(xira8!t$%guzfbr+80)up8b1s2oglk'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
